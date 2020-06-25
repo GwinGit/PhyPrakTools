254e100003c0bcb4dfd382b5ee42327c4b82b728
Tim Naschke <t.n.gwin13@gmail.com>
2020-06-25 17:04:37 +0200

    Restructured to fit with Python Package Style

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='phypraktools',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/GwinGit/PhyPrakTools.git',
+      author='Tim Naschke',
+      author_email='t.n.gwin13@gmail.com',
+      license='MIT',
+      packages=['phypraktools'],
+	  install_requires=['numpy','matplotlob'],
+      zip_safe=False)
